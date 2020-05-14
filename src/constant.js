af0d1604d68db28b0b6443f0c5823cc34356b5cb
XinjianLi <xinjian.li@syclarion.com>
2020-05-14 17:20:20 +0800

    配置生产，测试环境

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
 //测试环境
 // export const SERVICE_URL = { url:"http://localhost:8888"};
 //生产环境
-export const SERVICE_URL = { url:"http://192.168.141.128:8888"};
+export const SERVICE_URL = { url:"http://192.168.141.128:8080"};
 
